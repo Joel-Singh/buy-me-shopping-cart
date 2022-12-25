6e2922bdb9b129c39a2c76fb0e21aaaa07cb55a7
Joel Singh <joelsingh788@gmail.com>
2022-12-25 13:29:00 -0500

    Create and render home component

---
+++ src/components/Home.js
@@ -0,0 +1,9 @@
+function Home() {
+  return (
+    <div className="home">
+      <button type='button'>BUY ME!</ button>
+    </div>
+  )
+}
+
+export default Home;
--- src/components/RouteSwitch.js
+++ src/components/RouteSwitch.js
@@ -1,10 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Home";
 
 const RouteSwitch = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<div> Homepage </ div>} />
+        <Route path="/" element={<Home />} />
         <Route path="/profile" element={<div> Profile </ div>}/>
       </Routes>
     </BrowserRouter>
