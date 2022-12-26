78d24758cb1e8c613ac00c8d2cbec6f862237dcc
Joel Singh <joelsingh788@gmail.com>
2022-12-25 19:11:07 -0500

    Have BrowserRouter wrap around App in index.js rather than in App itself

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,13 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "./Home";
 import Store from "./Store"
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/store" element={<Store />}/>
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/store" element={<Store />}/>
+    </Routes>
   );
 };
 
