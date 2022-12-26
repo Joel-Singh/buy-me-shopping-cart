5fca749ea0f19cfa0fd8c6ef32f5e5383b000b8b
Joel Singh <joelsingh788@gmail.com>
2022-12-25 19:21:47 -0500

    Add Header component with proper Routing

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,20 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./Home";
 import Store from "./Store"
+import Header from "./Header.js"
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/store" element={<Store />}/>
-    </Routes>
+    <div className="app">
+      <Routes>
+        <Route path="/" element={null} />
+        <Route path="*" element={<Header />} />
+      </Routes>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/store" element={<Store />}/>
+      </Routes>
+    </div>
   );
 };
 
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <div className="header">
+      Buy Me
+    </div>
+  )
+}
+
+export default Header
