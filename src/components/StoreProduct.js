3a92769ff8f577e1002980fd3c59cf15a1cf4d37
Joel Singh <joelsingh788@gmail.com>
2022-12-30 10:18:10 -0500

    Format storeProduct.js

--- src/components/StoreProduct.js
+++ src/components/StoreProduct.js
@@ -1,15 +1,32 @@
 function StoreProduct(props) {
-  const { onAddToCartClick, onRemoveFromCartClick, amountInCart, productLimit = Infinity, image } = props;
+  const {
+    onAddToCartClick,
+    onRemoveFromCartClick,
+    amountInCart,
+    productLimit = Infinity,
+    image,
+  } = props;
+
   return (
     <div>
       <img src={image} />
+
       <button
         aria-label="Remove from cart"
         onClick={onRemoveFromCartClick}
         disabled={amountInCart === 0}
       ></button>
-      <span aria-label="Amount in cart" data-testid="Amount in cart">{amountInCart}</span>
-      <button aria-label="Add to cart" disabled={amountInCart === productLimit} onClick={onAddToCartClick}></button>
+
+      <span aria-label="Amount in cart" data-testid="Amount in cart">
+        {amountInCart}
+      </span>
+
+      <button
+        aria-label="Add to cart"
+        disabled={amountInCart === productLimit}
+        onClick={onAddToCartClick}
+      ></button>
+
     </div>
   );
 }
