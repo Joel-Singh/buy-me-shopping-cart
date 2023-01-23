8c717fea81a1ee775a4ae3c4a5f4d1dee56f7ba1
Joel Singh <joelsingh788@gmail.com>
2023-01-23 08:51:44 -0500

    Remove unnecessary import

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import Store from "./components/Store"
 import Header from "./components/Header.js"
-import CheckoutPreview from "./components/CheckoutPreview";
 
 function App() {
   return (
