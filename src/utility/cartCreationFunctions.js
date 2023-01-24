1d94ecfb66133445d9a55ab9b375afb7dbb8dbbe
Joel Singh <joelsingh788@gmail.com>
2023-01-24 11:25:24 -0500

    Add placeholder image to initial cart

--- src/utility/cartCreationFunctions.js
+++ src/utility/cartCreationFunctions.js
@@ -1,17 +1,18 @@
+import placeHolderImage from "../assets/placeholder.png"
 export function getInitialCart() {
   return (
     [
-      createCartItem('Glasses', 12, 0, 'glasses placeholder'),
-      createCartItem('Strand of my hair', 50, 0, 'Strand of my hair placeholder'),
-      createCartItem('Programmer Bath Water in a Bottle', 30, 0, 'Programmer Bath Water in a Bottle placeholder'),
-      createCartItem('A shirt with my face', 5, 0, 'A shirt with my face placeholder'),
-      createCartItem('My empty, used toothpaste tube', 30, 0, 'My empty, used toothpaste tube placeholder'),
-      createCartItem('My Used Razors', 30, 0, 'My Used Razors placeholder'),
-      createCartItem('Donation', 500, 0, 'Donation placeholder'),
-      createCartItem('Jared', 200, 0, 'Jared placeholder'),
-      createCartItem(`Gum i've chewed`, 200, 0, `Gum i've chewed placeholder`),
-      createCartItem('Lightly used pickle ;)', 200, 0, 'Lightly used pickle ;) placeholder'),
-      createCartItem('My pet dog', 200, 0, 'My pet dog placeholder')
+      createCartItem('Glasses', 12, 0, placeHolderImage),
+      createCartItem('Strand of my hair', 50, 0, placeHolderImage),
+      createCartItem('Programmer Bath Water in a Bottle', 30, 0, placeHolderImage),
+      createCartItem('A shirt with my face', 5, 0, placeHolderImage),
+      createCartItem('My empty, used toothpaste tube', 30, 0, placeHolderImage),
+      createCartItem('My Used Razors', 30, 0, placeHolderImage),
+      createCartItem('Donation', 500, 0, placeHolderImage),
+      createCartItem('Jared', 200, 0, placeHolderImage),
+      createCartItem(`Gum i've chewed`, 200, 0, placeHolderImage),
+      createCartItem('Lightly used pickle ;)', 200, 0, placeHolderImage),
+      createCartItem('My pet dog', 200, 0, placeHolderImage)
     ]
   )
 }
